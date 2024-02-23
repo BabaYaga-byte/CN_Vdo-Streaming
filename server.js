9b5067dfa55253b591aebfd4df5c9979b2162c8e
SouP <dakshsai299@gmail.com>
2024-02-23 10:11:15 +0530

    ohhyeahyeahpikyapuu
    -> deleted client.js
    -> merged client.js with html
    -> working on multiplt vides (I have a solution and IT WILL WORK DONT WORRY BE HAPPY)
    -> message to myself

--- server.js
+++ server.js
@@ -3,18 +3,42 @@ const fs = require('fs');
 const path = require('path');
 const WebSocket = require('ws');
 
-// Create an HTTP server to serve the video file
 const server = http.createServer((req, res) => {
-  const filePath = path.join(__dirname, 'video.mp4');
-  const stat = fs.statSync(filePath);
-
-  res.writeHead(200, {
-    'Content-Type': 'video/mp4',
-    'Content-Length': stat.size
-  });
-
-  const readStream = fs.createReadStream(filePath);
-  readStream.pipe(res);
+  // Serve the HTML file for the root URL
+  if (req.url === '/') {
+    const filePath = path.join(__dirname, 'index.html');
+    fs.readFile(filePath, (err, data) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200, { 'Content-Type': 'text/html' });
+      res.end(data);
+    });
+  }
+  // Serve the video file for the /video.mp4 path
+  else if (req.url === '/video.mp4') {
+    const videoPath = path.join(__dirname, 'video.mp4');
+    fs.stat(videoPath, (err, stat) => {
+      if (err) {
+        res.writeHead(404);
+        res.end(JSON.stringify(err));
+        return;
+      }
+      res.writeHead(200, {
+        'Content-Type': 'video/mp4',
+        'Content-Length': stat.size
+      });
+      const readStream = fs.createReadStream(videoPath);
+      readStream.pipe(res);
+    });
+  }
+  // Handle other paths
+  else {
+    res.writeHead(404);
+    res.end('Not Found');
+  }
 });
 
 // Create a WebSocket server and attach it to the HTTP server
